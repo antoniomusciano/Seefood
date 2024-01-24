7782aa214ab2ec4843ef72298b6fcebce5d04259
a <antoniomusciano@gmail.com>
2024-01-24 12:36:32 -0500

    clickable button

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import tkinter as tk
 window = tk.Tk()
 window.title("SeeFood")
 
+# Create a button
+button = tk.Button(window, text="Click Me!")
+button.pack(padx=200, pady=200)
+
 # Start the main loop
 window.mainloop()
